b8c044043b110cc44745023d87de85ef5c4af7d1
cxl0203 <1942613965@qq.com>
2019-04-10 13:34:06 +0800

    4.10

--- pinyougou-search-web/src/main/webapp/js/base.js
+++ pinyougou-search-web/src/main/webapp/js/base.js
@@ -1,2 +1,10 @@
 	//定义模块
 	var app = angular.module("pinyougou",[]);
+	/*$sce 服务写成过滤器*/
+	app.filter('trustHtml',['$sce',function($sce){
+		return function(data){
+			return $sce.trustAsHtml(data);
+		}
+	}]);
+
+	
--- pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -6,4 +6,26 @@ app.controller("searchController",function($scope,searchService){
 						$scope.resultMap=response;//搜索返回的结果
 					})
 	}
+	//关键字，分类，品牌，规格
+	$scope.searchMap={'keywords':'','category':'','brand':'','spec':{}};
+	//添加搜索项
+	$scope.addSearchItem = function(key,value){
+		//如果点击的是分类或者是品牌
+		if(key=='category' || key=='brand'){
+			$scope.searchMap[key] = value;
+		}else{
+			$scope.searchMap.spec[key] = value;
+		}
+		$scope.search();//执行搜索
+	}
+	//移除复合搜索条件
+	$scope.removeSearchItem=function(key){
+		if(key=="category" || key=="brand"){//如果是分类或品牌
+			$scope.searchMap[key]="";
+		}else{//否则是规格
+			delete $scope.searchMap.spec[key];//移除此属性
+			}
+		$scope.search();//执行搜索
+		}
+
 })
